a398fae973f5bc4662b7f170f4ff85df9159a117
Shane Logsdon <shane@shanelogsdon.com>
2015-02-02 23:41:31 -0500

    adding some acceptor tests

--- test/pool/acceptor_test.exs
+++ test/pool/acceptor_test.exs
@@ -1,4 +1,55 @@
 defmodule Pool.AcceptorTest do
   use ExUnit.Case, async: true
-end
+  alias Pool.Acceptor
+  alias Pool.Transport.Tcp
+  alias Test.Fixtures.EchoProtocol
 
+  @tcp_opts [:binary, {:active, false}]
+
+  test "accept/5 without spawn" do
+    {:ok, listen} = :gen_tcp.listen(0, @tcp_opts)
+    pid = Acceptor.start_link(listen, nil, Tcp, {EchoProtocol, []}, ref: :test_accept)
+    {:ok, port} = :inet.port(listen)
+
+    assert pid |> is_pid
+    assert pid |> Process.alive?
+
+    {:ok, socket} = :gen_tcp.connect({127,0,0,1}, port, @tcp_opts)
+    sent = :gen_tcp.send(socket, "Hi")
+
+    assert pid |> Process.alive?
+    assert sent == :ok
+  end
+
+  test "accept/5 with timeout" do
+    {:ok, listen} = :gen_tcp.listen(0, @tcp_opts)
+    pid = Acceptor.start_link(listen, nil, Tcp, {EchoProtocol, []}, ref: :test_accept,
+                                                                    accept_timeout: 2000)
+    {:ok, port} = :inet.port(listen)
+
+    assert pid |> is_pid
+    assert pid |> Process.alive?
+
+    :timer.sleep(2001) # A Space Odessy
+
+    {:ok, socket} = :gen_tcp.connect({127,0,0,1}, port, @tcp_opts)
+    sent = :gen_tcp.send(socket, "Hi")
+
+    assert pid |> Process.alive?
+    assert sent == :ok
+  end
+
+  test "accept/5 with exit" do
+    {:ok, listen} = :gen_tcp.listen(0, @tcp_opts)
+    pid = Acceptor.start_link(listen, nil, Tcp, {EchoProtocol, []}, ref: :test_accept)
+
+    assert pid |> is_pid
+    assert pid |> Process.alive?
+
+    # Trap exits here so the test runner doesn't exit
+    Process.flag(:trap_exit, true)
+    assert Process.exit(pid, :kill)
+    refute pid |> Process.alive?
+    assert_received {:'EXIT', ^pid, :killed}
+  end
+end
